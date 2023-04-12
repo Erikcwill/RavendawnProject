213a9e4002f8e6af96ca76641d4e2e0553ded43e
Erikcwill <erikcwill@gmail.com>
2023-04-12 16:09:54 -0300

    Complete modification of the base structure of the page. New branch usage test

--- script.js
+++ script.js
@@ -1,18 +1,51 @@
-import { handleSelect } from "./handleSelect.js";
 import { tradepacksList } from "./tradepacksList.js";
+import { ingredientList } from "./ingredientList.js";
 
+const tradePacksContainer = document.getElementById("tradePacksContainer");
 
-const select = document.getElementById("selecao");
-select.addEventListener("change", handleSelect);
+for (const tradepack of tradepacksList) {
+  const card = document.createElement("div");
+  card.className = "card_packs";
 
-function addOptionsToSelect() {
-  const select = document.getElementById("selecao");
-  tradepacksList.forEach((pack) => {
-    const option = document.createElement("option");
-    option.text = pack.name;
-    option.value = pack.name;
-    select.add(option);
-  });
+  const containerTitleCard = document.createElement("div");
+  containerTitleCard.className = "container_title_card";
+  card.appendChild(containerTitleCard);
+
+  const titleCard = document.createElement("h3");
+  titleCard.className = "title_card";
+  titleCard.textContent = `${tradepack.name}`;
+  containerTitleCard.appendChild(titleCard);
+
+  const ingredients = document.createElement("div");
+  ingredients.className = "card_ingredients";
+
+  for (const ingredient of tradepack.ingredients) {
+    const ingredientItem = document.createElement("p");
+    ingredientItem.className = "ingredient_item";
+    const ingredientInfo = ingredientList.find(
+      (info) => info.name === ingredient.name
+    );
+
+    ingredientItem.textContent = `${ingredient.name} - ${ingredient.quantity}`;
+    if (ingredientInfo) {
+      const price = parseInt(ingredientInfo.price);
+      const quantityRange = ingredientInfo.quantity.split("-");
+      const minQuantity = parseInt(quantityRange[0]);
+      const maxQuantity = parseInt(quantityRange[1]);
+      const unitsPerPack = ingredient.quantity;
+
+      const averageQuantity = (minQuantity + maxQuantity) / 2;
+      const costPerPack = Math.round((price / averageQuantity) * unitsPerPack);
+
+      const cost = costPerPack;
+
+      ingredientItem.innerHTML += ` Cost: ${cost} - ${ingredientInfo.category}: ${ingredientInfo.necessaryLevel}`;
+    } else {
+      ingredientItem.innerHTML += ` Cost: N/A - N/A`;
+    }
+    ingredients.appendChild(ingredientItem);
+  }
+  card.appendChild(ingredients);
+
+  tradePacksContainer.appendChild(card);
 }
-
-addOptionsToSelect();
--- tradepacksList.js
+++ tradepacksList.js
@@ -1,10 +1,10 @@
 export const tradepacksList = [
   {
-    name: "Seaforge Aged Fish",
+    name: "Bakers Basics",
     ingredients: [
-      { name: "Sliced Fish", quantity: "100" },
-      { name: "Acorn", quantity: "2" },
-      { name: "Orange", quantity: "3" },
+      { name: "Ground Flour", quantity: "35" },
+      { name: "Egg", quantity: "20" },
+      { name: "Milk", quantity: "15" },
     ],
   },
   {
@@ -16,12 +16,37 @@ export const tradepacksList = [
     ],
   },
   {
-    name: "Pie Making Kit",
+    name: "Sajecho Fruit Basket",
     ingredients: [
-      { name: "Apple", quantity: "5" },
-      { name: "Sunberry", quantity: "5" },
-      { name: "Cherry", quantity: "4" },
-      { name: "Ground Flour", quantity: "30" },
+      { name: "Watermelon", quantity: "1" },
+      { name: "Orange", quantity: "10" },
+      { name: "Grape", quantity: "40" },
+      { name: "Banana", quantity: "5" },
+    ],
+  },
+  {
+    name: "Basic Rations",
+    ingredients: [
+      { name: "Wheat", quantity: "40" },
+      { name: "Corn", quantity: "25" },
+      { name: "Apple", quantity: "15" },
+    ],
+  },
+  {
+    name: "Seabreeze Rum",
+    ingredients: [
+      { name: "Corn", quantity: "20" },
+      { name: "Cabbage", quantity: "8" },
+      { name: "Banana", quantity: "5" },
+      { name: "Blueberry", quantity: "30" },
+    ],
+  },
+  {
+    name: "Barbecue Specialty",
+    ingredients: [
+      { name: "Beef", quantity: "5" },
+      { name: "Chicken", quantity: "30" },
+      { name: "Coal", quantity: "10" },
     ],
   },
   {
@@ -42,46 +67,20 @@ export const tradepacksList = [
     ],
   },
   {
-    name: "Margrove Ale Ingredients",
+    name: "Seaforge Aged Fish",
     ingredients: [
-      { name: "Wheat", quantity: "30" },
-      { name: "Acorn", quantity: "3" },
-      { name: "Pumpkin", quantity: "2" },
+      { name: "Sliced Fish", quantity: "100" },
+      { name: "Acorn", quantity: "2" },
+      { name: "Orange", quantity: "3" },
     ],
   },
   {
-    name: "Juicer Box",
+    name: "Butchet's Box",
     ingredients: [
-      { name: "Apple", quantity: "5" },
-      { name: "Strawberry", quantity: "15" },
-      { name: "Cherry", quantity: "5" },
-      { name: "Banana", quantity: "3" },
-    ],
-  },
-  {
-    name: "Sombreshade's Pie",
-    ingredients: [
-      { name: "Pumpkin", quantity: "5" },
-      { name: "Milk", quantity: "10" },
-      { name: "Ground Flour", quantity: "20" },
-      { name: "Egg", quantity: "20" },
-    ],
-  },
-  {
-    name: "Vegetable Stew",
-    ingredients: [
-        { name: "Brocolli", quantity: "5" },
-        { name: "Pumpkin", quantity: "5" },
-        { name: "Beans", quantity: "25" },
-        { name: "Potato", quantity: "40" },
-    ],
-  },
-  {
-    name: "Winemakers Kit",
-    ingredients: [
-      { name: "Grape", quantity: "40" },
-      { name: "Blueberry", quantity: "15" },
-      { name: "Moonberry", quantity: "5" },
+      { name: "Chicken", quantity: "5" },
+      { name: "Beef", quantity: "5" },
+      { name: "Shank", quantity: "5" },
+      { name: "Cheese", quantity: "1" },
     ],
   },
   {
@@ -94,11 +93,194 @@ export const tradepacksList = [
     ],
   },
   {
-    name: "s",
+    name: "Dairy Delivery",
+    ingredients: [
+      { name: "Milk", quantity: "10" },
+      { name: "Cheese", quantity: "1" },
+      { name: "Egg", quantity: "30" },
+    ],
+  },
+  {
+    name: "Sombreshade's Pie",
+    ingredients: [
+      { name: "Pumpkin", quantity: "5" },
+      { name: "Milk", quantity: "10" },
+      { name: "Ground Flour", quantity: "20" },
+      { name: "Egg", quantity: "20" },
+    ],
+  },
+  {
+    name: "Exotic Fruit",
     ingredients: [
       { name: "Watermelon", quantity: "4" },
       { name: "Sunberry", quantity: "8" },
       { name: "Moonberry", quantity: "5" },
+    ],
+  },
+  {
+    name: "Fried Chicken",
+    ingredients: [
+      { name: "Chicken", quantity: "12" },
+      { name: "Onion", quantity: "10" },
+      { name: "Garlic", quantity: "10" },
+      { name: "Ground Flour", quantity: "20" },
+    ],
+  },
+  {
+    name: "Fruit Basket",
+    ingredients: [
+      { name: "Grape", quantity: "75" },
+      { name: "Watermelon", quantity: "2" },
+      { name: "Cherry", quantity: "12" },
+    ],
+  },
+  {
+    name: "Strawberry Cakes",
+    ingredients: [
+      { name: "Ground flour", quantity: "50" },
+      { name: "Milk", quantity: "10" },
+      { name: "Strawberry", quantity: "30" },
+    ],
+  },
+  {
+    name: "General Spices",
+    ingredients: [
+      { name: "Garlic", quantity: "15" },
+      { name: "Onion", quantity: "15" },
+      { name: "Pepper", quantity: "10" },
+      { name: "Salt Bowl", quantity: "5" },
+    ],
+  },
+  {
+    name: "Gilead Spiced Fish",
+    ingredients: [
+      { name: "Sliced Fish", quantity: "100" },
+      { name: "Pepper", quantity: "15" },
+      { name: "Onion", quantity: "15" },
+    ],
+  },
+  {
+    name: "Glaceforde Explorer Kit",
+    ingredients: [
+      { name: "wool", quantity: "10" },
+      { name: "coal", quantity: "40" },
+      { name: "log", quantity: "30" },
+    ],
+  },
+  {
+    name: "Vegetable Stew",
+    ingredients: [
+      { name: "Brocolli", quantity: "5" },
+      { name: "Pumpkin", quantity: "5" },
+      { name: "Beans", quantity: "25" },
+      { name: "Potato", quantity: "40" },
+    ],
+  },
+  {
+    name: "Juicer Box",
+    ingredients: [
+      { name: "Apple", quantity: "5" },
+      { name: "Strawberry", quantity: "15" },
+      { name: "Cherry", quantity: "5" },
+      { name: "Banana", quantity: "3" },
+    ],
+  },
+  {
+    name: "Kabbar's Omelets",
+    ingredients: [
+      { name: "Egg", quantity: "30" },
+      { name: "Cheese", quantity: "1" },
+      { name: "Pepper", quantity: "15" },
+    ],
+  },
+  {
+    name: "Kindling Kit",
+    ingredients: [
+      { name: "Log", quantity: "60" },
+      { name: "Coal", quantity: "40" },
+      { name: "Cotton", quantity: "10" },
+    ],
+  },
+  {
+    name: "Winemakers Kit",
+    ingredients: [
+      { name: "Grape", quantity: "40" },
+      { name: "Blueberry", quantity: "15" },
+      { name: "Moonberry", quantity: "5" },
+    ],
+  },
+  {
+    name: "Margrove Ale Ingredients",
+    ingredients: [
+      { name: "Wheat", quantity: "30" },
+      { name: "Acorn", quantity: "3" },
+      { name: "Pumpkin", quantity: "2" },
+    ],
+  },
+  {
+    name: "Pickled Fish",
+    ingredients: [
+      { name: "Sliced Fish", quantity: "100" },
+      { name: "Carrot", quantity: "40" },
+      { name: "Salt Bowl", quantity: "10" },
+    ],
+  },
+  {
+    name: "Pickled Vegetables",
+    ingredients: [
+      { name: "Carrot", quantity: "50" },
+      { name: "Cabbage", quantity: "10" },
+      { name: "Salt Bowl", quantity: "8" },
+    ],
+  },
+  {
+    name: "Building Materials",
+    ingredients: [
+      { name: "Log", quantity: "15" },
+      { name: "Stone", quantity: "50" },
+      { name: "Coal", quantity: "10" },
+    ],
+  },
+  {
+    name: "Pie Making Kit",
+    ingredients: [
+      { name: "Apple", quantity: "5" },
+      { name: "Sunberry", quantity: "5" },
+      { name: "Cherry", quantity: "4" },
+      { name: "Ground Flour", quantity: "30" },
+    ],
+  },
+  {
+    name: "Ravencrest Greens",
+    ingredients: [
+      { name: "Brocolli", quantity: "10" },
+      { name: "Pea", quantity: "25" },
+      { name: "Cabbage", quantity: "15" },
+    ],
+  },
+  {
+    name: "Rohna Smoked Ham",
+    ingredients: [
+      { name: "Shank", quantity: "5" },
+      { name: "Acorn", quantity: "2" },
+      { name: "Pea", quantity: "10" },
+      { name: "Salt Bowl", quantity: "5" },
+    ],
+  },
+  {
+    name: "Sailor's Remedy",
+    ingredients: [
+      { name: "Carrot", quantity: "20" },
+      { name: "Orange", quantity: "5" },
+      { name: "Beans", quantity: "20" },
+    ],
+  },
+  {
+    name: "Ravencrest Finest Wears",
+    ingredients: [
+      { name: "Wool", quantity: "8" },
+      { name: "Cotton", quantity: "8" },
+      { name: "Hide", quantity: "8" },
     ],
   },
 ];
