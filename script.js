872ab44c9dfe9d9d3a02196da329dd05c432ad70
Erick William <erikcwill@gmail.com>
2023-04-19 19:06:28 -0300

    Complete restructuring of the project to use selection input instead of creating a card for each source.

--- script.js
+++ script.js
@@ -5,6 +5,15 @@ const tradepackCardsContainer = document.getElementById(
   "tradepack-cards-container"
 );
 
+function createAndConfigureElement(elementType, classes = [], attributes = {}) {
+  const element = document.createElement(elementType);
+  classes.forEach((className) => element.classList.add(className));
+  Object.entries(attributes).forEach(([attributeName, attributeValue]) => {
+    element.setAttribute(attributeName, attributeValue);
+  });
+  return element;
+}
+
 for (const tradepack of tradepackList) {
   const card = document.createElement("div");
   card.classList.add("card");
@@ -15,20 +24,128 @@ for (const tradepack of tradepackList) {
 
   const ingredientsList = document.createElement("ul");
 
+  const totalPrice = document.createElement("div");
+  totalPrice.textContent = "Preço total: ";
+
   for (const ingredient of tradepack.ingredients) {
     const listItem = document.createElement("li");
+    listItem.classList.add("list_item");
+
     const ingredientData = ingredientList.find(
       (data) => data.name === ingredient.name
     );
     if (ingredientData) {
       const ingredientCategory = ingredientData.category;
       const ingredientName = ingredientData.name;
-      const ingredientQuantity = ingredient.quantity;
-      const ingredientLevel = ingredientData.necessaryLevel[0];
-      let ingredientPrice = ingredientData.price[0];
+      //Creating Name Container
+      const nameContainer = createAndConfigureElement("div", [
+        "name_container",
+      ]);
+      const nameValue = createAndConfigureElement("span", ["name_text"]);
+      //Creating Quantity Container
+      const quantityContainer = createAndConfigureElement("div", [
+        "quantity_container",
+      ]);
+      const quantityValue = createAndConfigureElement("span", [
+        "quantity_text",
+      ]);
+      //Creating Category & Level Container
+      const categoryContainer = createAndConfigureElement("div", [
+        "category_container",
+      ]);
+      const categoryValue = createAndConfigureElement("span", ["category_text",]);
+      const levelValue = createAndConfigureElement("span", ["level_text"]);
+      //Creating Price Container
+      const priceContainer = createAndConfigureElement("div", [
+        "price_container",
+      ]);
+      const priceText = createAndConfigureElement("span", ["price_text"]);
+      const priceValue = createAndConfigureElement("span", ["price_value"]);
+      //Creating Input
+      const input = document.createElement("select");
+      input.name = ingredientName;
+      input.id = `${tradepack.name}-${ingredientName}`;
 
-      const ingredientInfo = `${ingredientName} (${ingredientQuantity}), ${ingredientCategory}: ${ingredientLevel}, Preço: ${ingredientPrice} `;
-      listItem.textContent = `${ingredientInfo}`;
+      for (const source of ingredientData.source) {
+        const option = document.createElement("option");
+        option.value = source.name;
+        option.textContent = source.name;
+        input.appendChild(option);
+      }
+
+      const sourceSize = ingredientData.source.length;
+
+      if (sourceSize > 0) {
+        const firstSource = ingredientData.source[0];
+        const ingredientQuantity = ingredient.quantity;
+        const ingredientLevel = firstSource.necessaryLevel;
+        const ingredientPrice = firstSource.price;
+
+        nameValue.textContent = `${ingredientName}`;
+        quantityValue.textContent = `${ingredientQuantity}`;
+        categoryValue.textContent = `${ingredientCategory}: `;
+        levelValue.textContent = `${ingredientLevel}`;
+        priceText.textContent = "Preço: ";
+        priceValue.textContent = `${ingredientPrice}`;
+
+        listItem.appendChild(nameContainer);
+        listItem.appendChild(quantityContainer);
+        listItem.appendChild(categoryContainer);
+        listItem.appendChild(priceContainer);
+        listItem.appendChild(input);
+
+        nameContainer.appendChild(nameValue);
+        quantityContainer.appendChild(quantityValue);
+        categoryContainer.appendChild(categoryValue);
+        categoryContainer.appendChild(levelValue);
+        priceContainer.appendChild(priceText);
+        priceContainer.appendChild(priceValue);
+        
+      } else {
+        listItem.textContent = "N/A";
+      }
+
+      input.addEventListener("change", (event) => {
+        const selectedSource = ingredientData.source.find(
+          (source) => source.name === event.target.value
+        );
+
+        let ingredientPrice = "";
+        
+        let ingredientLevel = "";
+
+        if (selectedSource) {
+          
+          ingredientLevel = selectedSource.necessaryLevel;
+          ingredientPrice = selectedSource.price;
+          
+        }
+
+        const ingredientQuantity = ingredient.quantity;
+
+        nameValue.textContent = `${ingredientName}`;
+        quantityValue.textContent = `${ingredientQuantity}`;
+        categoryValue.textContent = `${ingredientCategory}: `;
+        levelValue.textContent = `${ingredientLevel}`;
+        priceText.textContent = "Preço: ";
+        priceValue.textContent = `${ingredientPrice}`;
+
+        listItem.appendChild(nameContainer);
+        listItem.appendChild(quantityContainer);
+        listItem.appendChild(categoryContainer);
+        listItem.appendChild(priceContainer);
+        listItem.appendChild(input);
+
+        nameContainer.appendChild(nameValue);
+        quantityContainer.appendChild(quantityValue);
+        categoryContainer.appendChild(categoryValue);
+        categoryContainer.appendChild(levelValue);
+        priceContainer.appendChild(priceText);
+        priceContainer.appendChild(priceValue);    
+           
+        listItem.appendChild(input);
+        
+      });
     } else {
       listItem.textContent = "N/A";
     }
@@ -36,75 +153,7 @@ for (const tradepack of tradepackList) {
     ingredientsList.appendChild(listItem);
   }
   card.appendChild(ingredientsList);
-
-  card.addEventListener("click", () => {
-    const popup = document.createElement("div");
-    popup.classList.add("popup");
-
-    for (const ingredient of tradepack.ingredients) {
-      const ingredientData = ingredientList.find(
-        (data) => data.name === ingredient.name
-      );
-      if (ingredientData) {
-        for (let i = 0; i < ingredientData.anotherSource.length; i++) {
-          const name = ingredientData.anotherSource[i];
-
-          const div = document.createElement("div");
-          div.classList.add("card");
-
-          const title = document.createElement("h2");
-          title.textContent = name;
-          div.appendChild(title);
-
-          const ingredientInfo = document.createElement("ul");
-
-          let ingredientesInfo = "";
-
-          for (const ingredientPack of tradepack.ingredients) {
-            const ingredientPackData = ingredientList.find(
-              (data) => data.name === ingredientPack.name
-            );
-            if (ingredientPackData) {
-              const ingredientPackCategory = ingredientPackData.category;
-              const ingredientPackName = ingredientPackData.name;
-              const ingredientPackQuantity = ingredientPack.quantity;
-              let ingredientPackLevel = ingredientPackData.necessaryLevel[0];
-              let ingredientPackPrice = ingredientPackData.price[0];
-
-              if (ingredientPackData.anotherSource.includes(name)) {
-                // Verifica se o name atual é encontrado em anotherSource
-                const index = ingredientPackData.anotherSource.indexOf(name);
-                ingredientPackPrice = ingredientPackData.anotherPrice[index];
-                ingredientPackLevel = ingredientPackData.necessaryLevel[index];
-              }
-
-              const ingredientPackInfo = `${ingredientPackName} (${ingredientPackQuantity}), ${ingredientPackCategory}: ${ingredientPackLevel}, Preço: ${ingredientPackPrice} `;
-              ingredientesInfo += `${ingredientPackInfo} \n`;
-            }
-          }
-
-          const listItem = document.createElement("li");
-          listItem.textContent = ingredientesInfo;
-          ingredientInfo.appendChild(listItem);
-
-          div.appendChild(ingredientInfo);
-
-          popup.appendChild(div);
-        }
-      }
-    }
-
-    const closeButton = document.createElement("button");
-    closeButton.textContent = "X";
-    closeButton.classList.add("close-button");
-    closeButton.addEventListener("click", () => {
-      popup.remove();
-    });
-
-    popup.appendChild(closeButton);
-
-    document.body.appendChild(popup);
-  });
+  card.appendChild(totalPrice);
 
   tradepackCardsContainer.appendChild(card);
 }
